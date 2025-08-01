50c4e04e3c7ee5a49ccb178262e596e4e12ac65e
Thibault Maekelbergh <thibault@thibtop.be>
2025-08-01 10:48:45 +0200

    Add macmon to pkgs, format file

--- home/packages.nix
+++ home/packages.nix
@@ -1,34 +1,36 @@
 { pkgs }:
 
-with pkgs; [
-	binwalk
-	btop
-	coreutils
-	croc
-	ctop
-	curl
-	d2
-	duf
-	ffmpeg
-	fzf
-	gh
-	glances
-	glow
-	gnumake
-	jq
-	lazydocker
-	lazygit
-	nano
-	ncdu
-	neofetch
-	netcat
-	nmap
-	rsync
-	screen
-	shellcheck
-	shfmt
-	ssh-copy-id
-	tmux
-	tree
-	wget
+with pkgs;
+[
+  binwalk
+  btop
+  coreutils
+  croc
+  ctop
+  curl
+  d2
+  duf
+  ffmpeg
+  fzf
+  gh
+  glances
+  glow
+  gnumake
+  jq
+  lazydocker
+  lazygit
+  macmon
+  nano
+  ncdu
+  neofetch
+  netcat
+  nmap
+  rsync
+  screen
+  shellcheck
+  shfmt
+  ssh-copy-id
+  tmux
+  tree
+  wget
 ]
